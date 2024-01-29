d5e2234801726c338abf52b3b3a376c487056cdc
Anwar Rahim <anwarrahim91@gmail.com>
2024-01-29 18:59:37 +0500

    commit

--- javaScript/function/passbyreferance.js
+++ javaScript/function/passbyreferance.js
@@ -10,9 +10,10 @@ const func2 = ()=>{
 
 //changing values
 const changesValues = (newA, newB) =>{
-    a = newA;
+    a = newA; 
     b = newB;
 }
+
 
 changesValues( 1,20);
 console.log(`This is the apdated value ${a}, ${b}`);
---
+++ javaScript/loops/forLoop.js
@@ -0,0 +1,32 @@
+// for loop
+/* Note for loop is the counter loop*/
+/* while and do while, conditional loop*/
+for(let i=0; i<10; i++){
+    if (i%2!=1){
+        continue;
+    }
+    else{
+        console.log("print the value "+ i);
+    }
+}
+
+// while loop
+let a= 0;
+while (a < 10){
+    if (a!==1){
+        console.log(a); 
+    }
+
+    a++;
+}
+
+// do while loop
+
+x=0;
+do{
+    if(x%2==0){
+
+    }
+}
+
+while()
